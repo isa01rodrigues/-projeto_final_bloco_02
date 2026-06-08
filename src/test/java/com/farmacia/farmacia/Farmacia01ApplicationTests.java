2b441c6b70db80db7b96dc6877d119e94d66cf15
Isabella Rodrigues <123326628+isa01rodrigues@users.noreply.github.com>
2026-06-08 10:02:57 -0300

    Iiciando Projeto

---
+++ src/test/java/com/farmacia/farmacia/Farmacia01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmacia.farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Farmacia01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
